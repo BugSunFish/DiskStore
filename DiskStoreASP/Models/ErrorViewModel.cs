fb0ea4b9bd2b7264301fdca9d96628d32cfb6bd1
Tutty <89860140+BugSunFish@users.noreply.github.com>
2023-08-24 00:35:48 +0300

    cleaned from frontend

--- DiskStoreASP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DiskStoreASP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

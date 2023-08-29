6690022719f832bfcb3bc9a4acadebe5c8e3cd71
Tutty <89860140+BugSunFish@users.noreply.github.com>
2023-08-29 19:44:08 +0300

    Get&Post

---
+++ DiskStoreAPI/Data/DataDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DiskStoreAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiskStoreAPI.Data
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+    }
+}
---
+++ DiskStoreAPI/Models/Track.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace DiskStoreAPI.Models
+{
+    public class Track
+    {
+        [Key]
+        [JsonIgnore]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string ImagePath { get; set; }
+        public int AuthorId { get; set; }
+    }
+}

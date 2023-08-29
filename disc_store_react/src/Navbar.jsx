052ae6613bdea26a7541bb27d4a8624d887d08ed
archeryfox <archeryfox2005@gmail.com>
2023-08-29 20:22:36 +0300

    clean for merge

--- disc_store_react/src/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import {Component} from "react";
-
-export class Navbar extends Component {
-	render() {
-		return (
-			<nav className={`border`} style={{height: `48em`, width: `25em`}}></nav>
-		)
-	}
-}
